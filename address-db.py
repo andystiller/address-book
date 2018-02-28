62136e3ed0eb301a41825d9cfe79ee104e92cd3b
Andy Stiller <andy@andyhat.co.uk>
2018-02-28 19:50:56 +0000

    Create inital database structure.

--- address-db.py
+++ address-db.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python3
-
+"""
+Address application database module
+This is the module that accesses the address book database for the main apllication.
+"""
 import sqlite3
 
-def createdb(curs):
+
+DATABASE_PATH = "data/addressbook.db"
+
+def createdb(conn):
     """
     Function to create the blank tables in the dabase.
-    curs is a cursor in a Sqlite database
+    con is a connection to a Sqlite database
     """
+    curs = conn.cursor()
 
     #Create Address table
-    curs.execute('''CREATE TABLE address
-             (first text, 
-             last text, 
-             phone text, 
-             email text,
-             address text
+    curs.execute('''CREATE TABLE address IF NOT EXISTS
+             (first_name TEXT, 
+             last_name TEXT, 
+             phone TEXT, 
+             email TEXT,
+             address TEXT
              )''')
+    # Save (commit) the changes
+    conn.commit()
+
+def table_exists(conn, table_name):
+    curs = conn.cursor()
+
+    #Create Address table
+    curs.execute('''SELECT name FROM sqlite_master WHERE type='table' AND name='?';'''),table_name
+    return curs.rowcount
+
+def main():
+    """
+    Entry point for testing if the file is run on it's own.
+    """
+    conn = sqlite3.connect(DATABASE_PATH)
+    createdb(conn)
+    conn.close
+
+if __name__ == "__main__":
+    main()
