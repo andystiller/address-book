92778ef35fcd2b697d741f9c65fe156219499c86
Andy Stiller <andy@andyhat.co.uk>
2018-03-04 19:43:21 +0000

    Finish functionality of address-cli.py and remove debug.

--- address-cli.py
+++ address-cli.py
@@ -15,20 +15,39 @@ def open_addressbook(filepath):
     if path_exists:
         with open(filepath, 'r') as infile:
             addressbook = json.load(infile)
-            print(addressbook)
 
     return addressbook
 
-def list_addresses(filepath):
+def list_contacts(filepath):
     addressbook = open_addressbook(filepath)
-    print(addressbook)
+    
+    if addressbook:
+        for contact in addressbook['contacts']:
+            print('Name: ' + contact['name'], 'Telephone: ' + contact['phone'], 'Address: ' + contact['address'], sep='\t', end='\n' )
+    else:
+        print('No address book found!')
 
-def find_address(filepath, contact):
-    pass
+def retrieve_contact(filepath, name):
+    contact_details = None
+    addressbook = open_addressbook(filepath)
+    print('Finding ', name)
 
-def add_address(filepath):
+    if addressbook:
+        for contact in addressbook['contacts']:
+            if contact['name'] == name:
+                contact_details = contact
+                break
+    else:
+        print('No address book found!')
+    
+    print('Name: ' + contact_details['name'], 'Telephone: ' + contact_details['phone'], 
+        'Address: ' + contact_details['address'], sep='\t', end='\n' )
+    return contact_details
+            
+
+
+def add_contact(filepath):
     new_contact = {}
-
     fullname = input('Please enter the contact name: ')
     print(fullname)
 
@@ -56,8 +75,22 @@ def add_address(filepath):
             json.dump(addressbook, outfile)
 
 
-def remove_address(filepath, contact):
-    pass
+def remove_contact(filepath, name):
+    # Have we got a name to remove?
+    if name:
+        # Try to open the current addressbook and find the contact index
+        addressbook = open_addressbook(filepath)
+        contact = retrieve_contact(filepath, name)
+
+        if contact:
+            with open(filepath, 'w') as outfile:
+                # Write the output file with the contact removed
+                print(outfile)
+                # If contact was found delete it
+                addressbook['contacts'].remove(contact)
+                print(addressbook)
+                json.dump(addressbook, outfile)
+                print('Contact: ', contact, ' removed.\n')
 
 def help():
     cmd_help = 'address-cli.py -h -i <input file> -l -a -r -f <contact to find>"'
@@ -69,7 +102,7 @@ def process_options(argv, inputfile):
     options = {}
 
     try:
-        opts, args = getopt.getopt(argv,"hi:larf:",["help","ifile=","list","add","remove","find="])
+        opts, args = getopt.getopt(argv,"hi:lar:f:",["help","ifile=","list","add","remove","find="])
     except getopt.GetoptError:
         help()
         sys.exit(2)
@@ -80,13 +113,13 @@ def process_options(argv, inputfile):
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
-            print(inputfile)
         elif opt in ("-l", "--list"):
             action = 'list'
         elif opt in ("-a", "--add"):
             action = 'add'
         elif opt in ("-r", "--remove"):
             action = 'remove'
+            contact = arg
         elif opt in ("-f", "--find"):
             action = 'find'
             contact = arg
@@ -103,19 +136,16 @@ def process_options(argv, inputfile):
 
 def main(argv):
     options = process_options(argv, FILE_PATH)
-    print(options['action'])
-    print(options['filepath'])
-    print(options['contact'])
 
     #Process the actions
     if options['action'] == 'list':
-        list_addresses(options['filepath'])
+        list_contacts(options['filepath'])
     elif options['action'] == 'add':
-        add_address(options['filepath'])
+        add_contact(options['filepath'])
     elif options['action'] == 'remove':
-        remove_address(options['filepath'], options['contact'])
+        remove_contact(options['filepath'], options['contact'])
     elif options['action'] == 'find':
-        find_address(options['filepath'], options['contact'])
+        retrieve_contact(options['filepath'], options['contact'])
         
 if __name__ == "__main__":
    main(sys.argv[1:])
